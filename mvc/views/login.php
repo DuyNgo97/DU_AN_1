8f74a2b52f9faa7784803ebe34ff226124797b2b
NQD <duynqps19550@fpt.edu.vn>
2022-07-12 13:21:58 +0700

    begin

--- mvc/views/login.php
+++ mvc/views/login.php
@@ -2,19 +2,122 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    <base href="http://localhost/da1/" target="_blank">
+    <link rel="stylesheet" href="./public/css/style.css" />
     <link rel="icon" href="public/images/logo.png">
-    <base href="http://localhost/DA1/" target="_blank">
-    <link rel="stylesheet" type="text/css" href="public/css/main.css">
-    <title>Login</title>
+    <title>Đăng nhập & Đăng ký</title>
 </head>
 
 <body>
-    <div class="content">
-        Trang login 1
+    <div class="container">
+        <div class="forms-container">
+            <div class="signin-signup">
+                <!-- form login -->
+                <form action="login/dangnhap" class="sign-in-form" target="_self" method="POST">
+
+                    <h2 class="title">Đăng nhập</h2>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="text" placeholder="Tài khoản" name="taikhoan" />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-lock"></i>
+                        <input type="password" placeholder="Mật khẩu" name="password" />
+                    </div>
+                    <input type="submit" value="Đăng nhập" class="btn solid" name="btn-dangnhap" />
+                    <p class="social-text">Hoặc Đăng nhập bằng các nền tảng xã hội</p>
+                    <div class="social-media">
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-google"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </div>
+                </form>
+                <!-- form dang ky -->
+                <form action="login/dangky" class="sign-up-form" target="_self" method="POST">
+                    <h2 class="title">Đăng ký</h2>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="text" placeholder="Họ và tên" name="name" />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="text" placeholder="Địa chỉ" name="diachi" />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="text" placeholder="Tài khoản" name="taikhoan" />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-envelope"></i>
+                        <input type="email" placeholder="Email" name="email" />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-lock"></i>
+                        <input type="password" placeholder="Mật khẩu" name="password" />
+                    </div>
+                    <input type="submit" class="btn" value="Đăng ký" name="btn-dangky" />
+                    <p class="social-text">Hoặc Đăng ký với các nền tảng xã hội</p>
+                    <div class="social-media">
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-google"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="panels-container">
+            <div class="panel left-panel">
+                <div class="content">
+                    <h3>Tạo tài khoản mới ở đây?</h3>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+                        ex ratione. Aliquid!
+                    </p>
+                    <button class="btn transparent" id="sign-up-btn">
+                        Đăng ký
+                    </button>
+                </div>
+                <img src="img/log.svg" class="image" alt="" />
+            </div>
+            <div class="panel right-panel">
+                <div class="content">
+                    <h3>Một trong số chúng tôi ?</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                        laboriosam ad deleniti.
+                    </p>
+                    <button class="btn transparent" id="sign-in-btn">
+                        Đăng nhập
+                    </button>
+                </div>
+                <img src="img/register.svg" class="image" alt="" />
+            </div>
+        </div>
     </div>
+
+    <script src="./public/js/app1.js"></script>
 </body>
 
 </html>
